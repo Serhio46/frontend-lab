f90d33e265103acfe52ff4749ed3ca7e9433c8d6
Sergei Shapovalov <1serhio46@gmail.com>
2021-09-27 10:31:19 +0300

    feat: made

---
+++ workflow/landing-page/js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu__icon').addEventListener('click', function (e) {
+	e.preventDefault();
+	//console.log("Hello");
+	document.querySelector(".resp-nav").classList.toggle("closenav")
+})
