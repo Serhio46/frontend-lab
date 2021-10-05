84e7e3ec7bc0597fb115072418e038108ee8afd5
Sergei Shapovalov <1serhio46@gmail.com>
2021-10-05 12:41:58 +0300

    feat: added dynamic loading of tabs

--- workflow/tabs/script.js
+++ workflow/tabs/script.js
@@ -1,22 +1,48 @@
+const tabs = [
+	{
+		title: 'JavaScript',
+		content: "JavaScript — это легковесный, интерпретируемый или JIT-компилируемый, объектно-ориентированный язык с функциями первого класса. Наиболее широкое применение находит как язык сценариев веб-страниц, но также используется и в других программных продуктах, например,node.js или Apache CouchDB. JavaScript это прототипно-ориентированный, мультипарадигменный язык сдинамической типизацией, который поддерживает объектно-ориентированный, императивный и декларативный например, функциональное программирование стили программирования.",
+	},
+	{
+		title: 'jQuery',
+		content: "jQuery — набор функций JavaScript, фокусирующийся на взаимодействии JavaScript и HTML. Библиотека jQuery помогает легко получать доступ к любому элементу DOM, обращаться к атрибутам и содержимому элементов DOM, манипулировать ими. Также библиотека jQuery предоставляет удобный API для работы с AJAX.",
+	},
+	{
+		title: 'DOM',
+		content: "DOM (от англ. Document Object Model — «объектная модель документа») — это независящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов.",
+	},
+	{
+		title: 'CSS',
+		content: "CSS («каскадные таблицы стилей») — формальный язык описания внешнего вида документа (веб-страницы), написанного с использованием языка разметки (чаще всего HTML или XHTML). Также может применяться к любым XML-документам, например, к SVG или XUL."
+	},
+];
+
 const cartText = document.querySelector('.cart__text');
-const tabs = document.querySelectorAll('li');
 
-const content = {
-	JS: "JavaScript — это легковесный, интерпретируемый или JIT-компилируемый, объектно-ориентированный язык с функциями первого класса. Наиболее широкое применение находит как язык сценариев веб-страниц, но также используется и в других программных продуктах, например,node.js или Apache CouchDB. JavaScript это прототипно-ориентированный, мультипарадигменный язык сдинамической типизацией, который поддерживает объектно-ориентированный, императивный и декларативный например, функциональное программирование стили программирования.",
-	JQ: "jQuery — набор функций JavaScript, фокусирующийся на взаимодействии JavaScript и HTML. Библиотека jQuery помогает легко получать доступ к любому элементу DOM, обращаться к атрибутам и содержимому элементов DOM, манипулировать ими. Также библиотека jQuery предоставляет удобный API для работы с AJAX.",
-	DOM: "DOM (от англ. Document Object Model — «объектная модель документа») — это независящий от платформы и языка программный интерфейс, позволяющий программам и скриптам получить доступ к содержимому HTML-, XHTML- и XML-документов, а также изменять содержимое, структуру и оформление таких документов.",
-	CSS: "CSS («каскадные таблицы стилей») — формальный язык описания внешнего вида документа (веб-страницы), написанного с использованием языка разметки (чаще всего HTML или XHTML). Также может применяться к любым XML-документам, например, к SVG или XUL."
-}
+function makeTabs(tabs) {
+	const cartNav = document.querySelector('.cart__nav');
+	for (let i = 0; i < tabs.length; i++) {
+		const cartNavItem = document.createElement('li');
+		cartNavItem.addEventListener('click', (event) => {
+			changeTab(event.target);
+		})
+		if (i === 0) {
+			cartNavItem.classList.add('active');
+			cartText.innerHTML = tabs[i].content;
+		}
+		cartNavItem.innerHTML = tabs[i].title;
+		cartNav.appendChild(cartNavItem);
+	}
+};
 
-Array.from(tabs).forEach((elem) => {
-	elem.addEventListener('click', (event) => {
-		changeTab(event.target);
-	})
-})
+makeTabs(tabs);
 
 function changeTab(btn) {
-	Array.from(tabs).forEach((elem) => elem.classList.remove('active'));//убрали у всех класс active
+	const allTabs = document.querySelectorAll('li');
+	Array.from(allTabs).forEach((elem) => elem.classList.remove('active'));
 	btn.classList.add('active');
-	cartText.innerHTML = content[btn.id];
+	const key = btn.innerHTML;
+	const item = tabs.find(elem => elem.title === key);
+	cartText.innerHTML = item.content;
 }
 
