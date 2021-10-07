eb2e9361918f8fe5543f65924d6d6fe7053e0f22
Sergei Shapovalov <1serhio46@gmail.com>
2021-10-07 15:00:43 +0300

    feat: validation JSON

--- workflow/json/script.js
+++ workflow/json/script.js
@@ -4,35 +4,35 @@ const json = {
 	"friends": ["Vasil", "Den", "Gringo"],
 	"object": {
 		"a": "b",
-		"c": "d",
-		"a": {
-			"f": "f"
-		}
+		"c": "d"
 	},
 	"string": "Hello"
 };
 
+const textArea = document.querySelector('.input');
 const btn = document.querySelector('.content__btn');
+btn.addEventListener('click', buildTree);
 
 function addElem(json) {
+	let id = 0;
 	const output = document.querySelector('.output');
 	const object = document.createElement('div');
 	object.classList.add('object-high');
 	object.innerHTML = `
 			<div class="elem-title">
-				<div><i class="fas fa-caret-down"></i></div>
+				<div class="btn" id='${id}' ><i class="fas fa-caret-down" id='${id}'></i></div>
 				<div class="object-title">BigObject</div>
 				<div>{${Object.keys(json).length}}</div>
 			</div>
+			<div class="obj-content" id='${id}'></div>
 			`;
+	id++;
 	output.appendChild(object);
-
-	const objHeigh = document.querySelector('.object-high');
-
-	parse(json, objHeigh);
+	const objHeigh = document.querySelector('.obj-content');
+	parse(json, objHeigh, id);
 }
 
-function parse(elems, block) {
+function parse(elems, block, id) {
 
 	for (let key in elems) {
 
@@ -41,13 +41,15 @@ function parse(elems, block) {
 			object.classList.add('object-low');
 			object.innerHTML = `
 				<div class="elem-title">
-					<div><i class="fas fa-caret-down"></i></div>
+					<div class="btn" id='${id}'><i class="fas fa-caret-down" id='${id}'></i></div>
 					<div class="object-title">${key}</div>
 					<div>${Array.isArray(elems[key]) ? `[${Object.keys(elems[key]).length}]` : `{${Object.keys(elems[key]).length}}`}</div >
 				</div >
+				<div class="obj-content" id='${id}'></div>
 				`;
+			id++;
 			block.appendChild(object);
-			const newBlock = Array.from(document.querySelectorAll('.object-low')).pop();
+			const newBlock = Array.from(document.querySelectorAll('.obj-content')).pop();
 			parse(elems[key], newBlock);
 		} else {
 			const primitive = document.createElement('div');
@@ -55,7 +57,6 @@ function parse(elems, block) {
 			primitive.innerHTML = `${key} : <span class="type">${elems[key]}</span>`;
 			block.appendChild(primitive);
 			const type = Array.from(document.querySelectorAll('.type')).pop();
-			console.log(elems[key]);
 			switch (typeof elems[key]) {
 				case 'number':
 					type.classList.add('number');
@@ -71,20 +72,29 @@ function parse(elems, block) {
 }
 
 function buildTree() {
-	const input = document.querySelector('.input');
-	const middleResult = JSON.parse(input.value);
-	console.log(middleResult);
+	//const input = document.querySelector('.input');
+	const middleResult = JSON.parse(textArea.value);
 	addElem(middleResult);
+	const buttons = document.querySelectorAll('.btn');
+	Array.from(buttons).forEach(elem => {
+		elem.addEventListener('click', () => collaps(elem.id));
+	});
 }
 
-btn.addEventListener('click', buildTree);
+function collaps(id) {
+	const blocks = document.querySelectorAll('.obj-content');
+	const icons = document.querySelectorAll('.fa-caret-down');
+	const change = Array.from(blocks).find(elem => elem.id === id);
+	const changeIcon = Array.from(icons).find(elem => elem.id === id);
+	change.classList.toggle('hidden');
+	changeIcon.classList.toggle('fa-caret-right');
+}
 
-const textArea = document.querySelector('.input');
+//Табуляция
 textArea.addEventListener('keydown', function (event) {
 	if (event.key === 'Tab') {
-		console.log(this)
+		console.log('tab')
 		event.preventDefault();
-		console.log('hello')
 		let value = '';
 		const nextSelectionStart = this.selectionStart + 4;
 
@@ -97,3 +107,23 @@ textArea.addEventListener('keydown', function (event) {
 		this.selectionEnd = nextSelectionStart;
 	}
 })
+
+textArea.addEventListener('input', validation);
+
+function validation() {
+	const validation = document.querySelector('.validation');
+	try {
+		const a = JSON.parse(textArea.value);
+		validation.innerHTML = '';
+		validation.classList.remove('jsonOn');
+		console.log(a);
+	} catch (e) {
+		validation.innerHTML = 'Ошибка ввода JSON';
+		validation.classList.add('jsonOn');
+	}
+	//console.log(JSON.parse(textArea.value));
+}
+
+
+
+
