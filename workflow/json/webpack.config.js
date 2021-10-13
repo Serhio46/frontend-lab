7201fe0aee6c9ba567750c06e7eecd53f0451d0d
Sergei Shapovalov <1serhio46@gmail.com>
2021-10-13 13:07:10 +0300

    feat: changed webpack.config: output.filename and scss loader

--- workflow/json/webpack.config.js
+++ workflow/json/webpack.config.js
@@ -1,18 +1,32 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const isDev = process.env.NODE_ENV === 'development';
+const isProd = !isDev;
+
+const fileName = (ext) => isDev ? `[name].${ext}` : `[name].[contenthash].${ext}`;
 
 module.exports = {
 	mode: "development",
 	entry: "./src/script.js",
 	output: {
-		filename: "bundle.js",
+		filename: `${fileName('js')}`,
 		path: path.resolve(__dirname, "dist"),
 	},
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: "./public/index.html",
+			template: path.resolve(__dirname, "./public/index.html"),
 			scriptLoading: "blocking",
+			minify: {
+				collapseWhitespace: isProd,
+			}
 		}),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: `${fileName('css')}`
+		})
 	],
 	module: {
 		rules: [
@@ -28,11 +42,11 @@ module.exports = {
 			},
 			{
 				test: /\.css$/i,
-				use: ["style-loader", "css-loader"],
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
 			},
 			{
 				test: /\.s[ac]ss$/i,
-				use: ["style-loader", "css-loader", "sass-loader"],
+				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
 			},
 		],
 	},
